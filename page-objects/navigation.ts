b97a151d1e163de284ec766dad83c09b7ef51d8a
Joboy Joseph <joboyjoseph@gmail.com>
2025-11-24 15:58:40 +0530

    yarn lint

--- page-objects/navigation.ts
+++ page-objects/navigation.ts
@@ -6,17 +6,17 @@ export class NavigationPage {
     this.page = page;
   }
   async formLayoutsPage() {
-  await this.page.getByRole('link', { name: 'Forms' }).click();
-  await this.page.getByRole('link', { name: 'Form Layouts' }).click();
+    await this.page.getByRole('link', { name: 'Forms' }).click();
+    await this.page.getByRole('link', { name: 'Form Layouts' }).click();
   }
 
   async datePickerPage() {
-  await this.page.getByRole('link', { name: 'Forms' }).click();
-  await this.page.getByRole('link', { name: 'Datepicker' }).click();
-  await this.page.getByRole('textbox', { name: 'Form Picker' }).click();
-  await this.page.getByText('24').click();
-  await this.page.getByRole('textbox', { name: 'Range Picker' }).click();
-  await this.page.getByRole('button').nth(4).click();
-  await this.page.getByText('11', { exact: true }).click();
+    await this.page.getByRole('link', { name: 'Forms' }).click();
+    await this.page.getByRole('link', { name: 'Datepicker' }).click();
+    await this.page.getByRole('textbox', { name: 'Form Picker' }).click();
+    await this.page.getByText('24').click();
+    await this.page.getByRole('textbox', { name: 'Range Picker' }).click();
+    await this.page.getByRole('button').nth(4).click();
+    await this.page.getByText('11', { exact: true }).click();
   }
 }
--- tests/0001usePageObjects.spec.ts
+++ tests/0001usePageObjects.spec.ts
@@ -5,16 +5,15 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/');
 });
 
-test.describe('Regression', ()=> {
-test('navigate to form page', async ({ page }) => {
+test.describe('Regression', () => {
+  test('navigate to form page', async ({ page }) => {
     const navigateTo = new NavigationPage(page);
     await navigateTo.formLayoutsPage();
-  })
-})
-  test.describe('Smoke', () => {
-    test('navigate to date picker page', async ({ page }) => { 
-      const navigateTo = new NavigationPage(page)
-      await navigateTo.datePickerPage();
-      
-  })
-})
+  });
+});
+test.describe('Smoke', () => {
+  test('navigate to date picker page', async ({ page }) => {
+    const navigateTo = new NavigationPage(page);
+    await navigateTo.datePickerPage();
+  });
+});
